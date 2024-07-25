e5ba5084acee1c8fe9ace5de480dc69b3666366c
Renan <renan.cruz10@gmail.com>
2024-07-25 20:03:02 -0300

    second commit

---
+++ Lexer/Tag.java
@@ -0,0 +1,7 @@
+public class Tag{
+
+    // Palavras reservadas
+    public final static int app = 256;
+    public final static int var = 257;
+    
+}
---
+++ Lexer/Token.java
@@ -0,0 +1,10 @@
+class Token{
+
+    private int tag;
+
+    Token(int t){
+        tag = this.tag;
+    }
+
+    
+}
